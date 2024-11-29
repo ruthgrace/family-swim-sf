56cb6f5447b9c1a060879ce72c228ffbfa9a7342
Ruth Grace Wong <ruthgracewong@gmail.com>
2024-11-29 23:30:25 +0000

    update swim map 2024-11-29

--- frontend/src/ControlPanel.tsx
+++ frontend/src/ControlPanel.tsx
@@ -4,7 +4,7 @@ import { Drawer } from "vaul";
 
 const snapPoints = ["245px", "350px"];
 
-const updatedAt = "2024-11-03";
+const updatedAt = "2024-11-29";
 
 export const ControlPanel = ({
   selectedDay,
