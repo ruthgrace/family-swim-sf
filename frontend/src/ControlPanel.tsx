c5c72b6a3fba357ad2092aa30b19cd5089e33a8d
Ruth Grace Wong <ruthgracewong@gmail.com>
2024-12-24 00:39:12 +0000

    update swim map data for 2024-12-23

--- frontend/src/ControlPanel.tsx
+++ frontend/src/ControlPanel.tsx
@@ -5,7 +5,7 @@ import { Weekday } from "./poolDataTypes";
 
 const snapPoints = ["245px", "350px"];
 
-const updatedAt = "2024-11-30";
+const updatedAt = "2024-12-23";
 
 export const ControlPanel = ({
   selectedDay,
