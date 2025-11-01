1f8a43e0ff8560d7eb7ccb0b4ce56f7077da7c0b
ruthgrace <ruthgracewong@gmail.com>
2025-11-01 23:52:15 +0000

    update swim map data for 2025-11-01

--- frontend/src/ControlPanel.tsx
+++ frontend/src/ControlPanel.tsx
@@ -5,7 +5,7 @@ import { Weekday } from "./poolDataTypes";
 
 const snapPoints = ["245px", "350px"];
 
-const updatedAt = "2025-10-30";
+const updatedAt = "2025-11-01";
 
 export const ControlPanel = ({
   selectedDay,
